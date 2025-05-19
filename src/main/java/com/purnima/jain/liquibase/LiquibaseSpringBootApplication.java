52d199c38e831fd5885c842e1f9dd31bf2f5cc8b
Purnima Jain <jain_purnima@hotmail.com>
2025-05-19 22:19:38 +0530

    Initialize Spring Boot application

---
+++ src/main/java/com/purnima/jain/liquibase/LiquibaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.purnima.jain.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purnima/jain/liquibase/LiquibaseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purnima.jain.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class LiquibaseSpringBootApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
